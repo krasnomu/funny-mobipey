c379d5fb84e41df68d2316cdc1e99a813d2a55f7
krasnomu <122415106+krasnomu@users.noreply.github.com>
2020-07-04 14:39:33 +0000

    Remove backup files

--- aligner/wavfile.py
+++ aligner/wavfile.py
@@ -1,1 +1,4 @@
-# 
+# Copyright (c) 2011-2014 Kyle Gorman and Michael Wagner
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation file
