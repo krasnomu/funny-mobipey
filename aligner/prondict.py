3e997329d9441dc5f145c4ca81ad99d2fec30300
krasnomu <122415106+krasnomu@users.noreply.github.com>
2020-07-04 09:41:14 +0000

    ASoC: dapm: Fix source list debugfs outputs

--- aligner/prondict.py
+++ aligner/prondict.py
@@ -19,3 +19,71 @@
 # CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+Pronunciation dictionary utilities
+"""
+
+
+import logging
+
+from collections import defaultdict
+
+from .utilities import SIL, SP
+
+
+class PronDict(object):
+
+    """
+    A wrapper for a normal pronunciation dictionary in the CMU style
+    """
+
+    SILENT_PHONES = frozenset([SIL, SP])
+
+    @staticmethod
+    def pronify(source):
+        for (i, line) in enumerate(source, 1):
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            try:
+                (word, pron) = line.split(None, 1)
+            except ValueError:
+                logging.error("Formatting error in dictionary '{}' (ln. {}).".format(source.name, i))
+                exit(1)
+            yield (i, word, pron.split())
+
+    def add(self, filename):
+        # build up dictionary
+        with open(filename, "r") as source:
+            for (i, word, pron) in PronDict.pronify(source):
+                for ph in pron:
+                    if ph not in self.ps | self.SILENT_PHONES:
+                        logging.error("Unknown phone '{}' in dictionary '{}' (ln. {}).".format(ph, filename, i))
+                        exit(1)
+                self.d[word].append(pron)
+
+    def __init__(self, phoneset, filename=None):
+        self.ps = phoneset
+        self.d = defaultdict(list)
+        if filename:
+            self.add(filename)
+        # for later...
+        self.oov = set()
+
+    def __contains__(self, key):
+        return key in self.d and self.d[key] != []
+
+    def __getitem__(self, key):
+        getlist = self.d[key]
+        if getlist:
+            return getlist
+        else:
+            self.oov.add(key)
+            raise KeyError(key)
+
+    def __repr__(self):
+        return "PronDict({})".format(self.d)
+
+    def __setitem__(self, key, value):
+        self.d[key].append(value)
