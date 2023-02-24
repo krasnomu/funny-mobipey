0e52907efba683fd3b1d02a3f8e3aa4044086088
krasnomu <122415106+krasnomu@users.noreply.github.com>
2023-02-24 05:28:34 +0000

    temp.

--- resample.sh
+++ resample.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-# resample.sh: resample audio usin
+# resample.sh: resample audio using SoX
+# Kyle Gorman <gormanky@ohsu.edu>
+
+set -e
+
+SOX=sox
+EXT=wav
+SOXCMD="$SOX -G "%s" -b 16 "%s" remix - rate "%d" dither -s"
+
+SAMPLERATES=(4000 8000 10000 11025 12500 15625 16000
